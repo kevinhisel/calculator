ecf1329eb1296af0cd12bf1c45e1e59f91a510e4
Kevin Hisel <kevinhisel@gmail.com>
2021-10-28 02:05:42 -0500

    Add functionality for buttons to perform basic math operations

--- script.js
+++ script.js
@@ -1,13 +1,54 @@
-const btns    = document.querySelectorAll('.numeral');
-const display = document.querySelector('#display');
-const text    = document.createElement('p');
+const calculator = {
+  newNumber  : true,
+  hasDecimal : false,
+};
 
-btns.forEach(btn => {
-  btn.addEventListener('click', () => {
-    text.textContent += btn.textContent;
-    display.appendChild(text);
+wireButtons();
+
+function wireButtons() {
+  const btns    = document.querySelectorAll('button');
+  const display = document.querySelector('#display');
+  const text    = document.querySelector('#display > p');
+  
+  btns.forEach(btn => {
+    if (btn.name === 'number') {
+      btn.addEventListener('click', () => {
+        if (calculator.newNumber || text.textContent === '0') {
+          text.textContent = '';
+          calculator.newNumber = false;
+        }
+        text.textContent += btn.textContent;
+        display.appendChild(text);
+      });
+    } else if (btn.name === 'operator') {
+      btn.addEventListener('click', () => {
+        calculator.firstNumber = Number(text.textContent);
+        calculator.operator    = btn.value;
+        readyNew();
+        
+      });
+    } else if (btn.name === 'equals') {
+      btn.addEventListener('click', () => {
+        text.textContent = 
+            operate(calculator.operator, calculator.firstNumber, Number(text.textContent));
+        display.appendChild(text);
+        readyNew();
+      });
+    } else if (btn.name === 'decimal') {
+      btn.addEventListener('click', () => {
+        if(!calculator.hasDecimal) {
+          if (calculator.newNumber) {
+            text.textContent = '0';
+            calculator.newNumber = false;
+          }
+          text.textContent += btn.textContent;
+          display.appendChild(text);
+          calculator.hasDecimal = true;
+        }
+      });
+    }
   });
-})
+}
 
 function add(a, b) {
   return a + b;
@@ -30,4 +71,9 @@ function operate(operator, a, b) {
   else if (operator === '-') return subtract(a, b);
   else if (operator === '*') return multiply(a, b);
   else if (operator === '/') return divide(a, b);
+}
+
+function readyNew() {
+  calculator.newNumber  = true;
+  calculator.hasDecimal = false;
 }
